936c02dcf23d037af2bc8f63bdb1d7a80ce3c8df
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2023-08-29 16:38:39 +0300

    Delete search-all.py

--- search-all.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch import Elasticsearch
-
-es = Elasticsearch("http://localhost:9200")
-
-
-resp = es.search(index="books", query={"match_all": {}})
-print("Got %d Hits:" % resp['hits']['total']['value'])
-for hit in resp['hits']['hits']:
-    print(hit)
