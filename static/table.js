8b7886fadfd118dc428c55f4085d3d21d7475c87
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2023-09-27 13:59:22 +0300

    Allow dynamically ordering A/B columns using a variable

--- static/table.js
+++ static/table.js
@@ -1,4 +1,4 @@
-async function generateBooksTable() {
+async function generateBooksTable(reversed) {
     try {
         const response = await fetch('/searched_books');
         const data = await response.json();
@@ -6,14 +6,18 @@ async function generateBooksTable() {
         const table = $('#books-table');
         table.empty(); // Clear existing rows
 
-        // Generate and insert the table head
+        // Determine the order of the "Found in A" and "Found in B" columns based on 'reversed'
+        // const reversed = true; // Set to true or false based on your logic
+        const columnOrder = reversed ? ['Found in B', 'Found in A'] : ['Found in A', 'Found in B'];
+
+        // Generate and insert the table head with the determined column order
         const tableHead = $('<thead>').html(`
             <tr>
                 <th>#</th>
                 <th>Title</th>
                 <th>Author(s)</th>
-                <th>Found in A</th>
-                <th>Found in B</th>
+                <th>${columnOrder[0]}</th>
+                <th>${columnOrder[1]}</th>
             </tr>
         `);
         table.append(tableHead);
@@ -27,13 +31,25 @@ async function generateBooksTable() {
             const isFoundAColor = book.is_found_a ? 'green' : 'red';
             const isFoundBColor = book.is_found_b ? 'green' : 'red';
 
-            row.html(`
-                <td>${rowNumber}</td>
-                <td>${book.title}</td>
-                <td>${book.authors}</td>
-                <td style="color: ${isFoundAColor}; font-size: 25px; text-align: center;">${isFoundAIcon}</td>
-                <td style="color: ${isFoundBColor}; font-size: 25px; text-align: center;">${isFoundBIcon}</td>
-            `);
+            // Populate the row based on the determined column order
+            if (reversed) {
+                row.html(`
+                    <td>${rowNumber}</td>
+                    <td>${book.title}</td>
+                    <td>${book.authors}</td>
+                    <td style="color: ${isFoundBColor}; font-size: 25px; text-align: center;">${isFoundBIcon}</td>
+                    <td style="color: ${isFoundAColor}; font-size: 25px; text-align: center;">${isFoundAIcon}</td>
+                `);
+            } else {
+                row.html(`
+                    <td>${rowNumber}</td>
+                    <td>${book.title}</td>
+                    <td>${book.authors}</td>
+                    <td style="color: ${isFoundAColor}; font-size: 25px; text-align: center;">${isFoundAIcon}</td>
+                    <td style="color: ${isFoundBColor}; font-size: 25px; text-align: center;">${isFoundBIcon}</td>
+                `);
+            }
+
             table.append(row);
 
             rowNumber++; // Increment the row number for the next row
